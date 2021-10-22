9bdbc9b95501dfe7ef857c8238056856f24bc0ee
erib <eri-b@users.noreply.github.com>
2021-10-22 18:28:35 -0400

    more schema

---
+++ app/models/group.rb
@@ -0,0 +1,3 @@
+class Group < ApplicationRecord
+  belongs_to :creator, class_name: 'User'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_194506) do
+ActiveRecord::Schema.define(version: 2021_10_22_220641) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.integer "creator_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["creator_id"], name: "index_groups_on_creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "phone_number"
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_10_22_194506) do
     t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
   end
 
+  add_foreign_key "groups", "users", column: "creator_id"
 end

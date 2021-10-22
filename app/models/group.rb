09a8e5ecf9ae021d97a910a0ae48b3198812a96d
erib <eri-b@users.noreply.github.com>
2021-10-22 18:50:55 -0400

    chipping

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ApplicationRecord
   belongs_to :creator, class_name: 'User'
+  validates :name, allow_blank: true, length: { in: 2..25 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  has_many :groups
+  has_many :groups, foreign_key: "creator_id"
+  validates :phone_number, presence: true, uniqueness: true
+  validates :username, allow_blank: true, length: { in: 3..25 }
 end
